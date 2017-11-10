5a8989fedc1885a9a977cff3517b9d2a5d17119b
lash <dev@holbrook.no>
2017-11-10 01:22:17 +0100

    swarm/storage: Correct datadir for blockchain API call

--- swarm/storage/resource_test.go
+++ swarm/storage/resource_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/ethereum/go-ethereum/eth"
 	"github.com/ethereum/go-ethereum/log"
@@ -16,9 +17,14 @@ func init() {
 }
 
 func TestResourceHandler(t *testing.T) {
-
+	datadir, err := ioutil.TempDir("", "rh")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(datadir)
 	cfg := &node.DefaultConfig
 	cfg.IPCPath = "geth.ipc"
+	cfg.DataDir = datadir
 	stack, err := node.New(cfg)
 	if err != nil {
 		t.Fatalf("ouch %v", cfg)
@@ -33,17 +39,19 @@ func TestResourceHandler(t *testing.T) {
 	stack.Start()
 	defer stack.Stop()
 
-	datadir, err := ioutil.TempDir("", "rh")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(datadir)
 	d, err := NewLocalDPA(datadir)
 	if err != nil {
 		t.Fatal(err)
 	}
 	d.Start()
 	defer d.Stop()
+	for {
+		_, err := os.Stat(stack.IPCEndpoint())
+		if err == nil {
+			break
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
 	rh := NewResourceHandler(stack, d)
 	_ = rh.NewResource("foo")
 	rh.hasher.Reset()
@@ -61,3 +69,20 @@ func TestResourceHandler(t *testing.T) {
 		t.Fatalf("Retrieved chunk data does not match input hash, expected %x, got %x", key, chunk.SData)
 	}
 }
+
+//func makeDPA() (*DPA, error) {
+//	hasher := MakeHashFunc("SHA256")
+//	tmpdir, err := ioutil.TempDir("", "resourcesync")
+//	if err != nil {
+//		return nil, err
+//	}
+//	storeparams := NewStoreParams(tmpdir)
+//	chunkerparams := NewChunkerParams()
+//	localstore, err := NewLocalStore(hasher, storeparams)
+//	if err != nil {
+//		return nil, err
+//	}
+//	netstore := NewNetStore(hasher, localstore, nil, storeparams)
+//	chunkstore := NewDpaChunkStore(localstore, netstore)
+//	return NewDPA(chunkstore, chunkerparams), nil
+//}
