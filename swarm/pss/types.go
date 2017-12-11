94fd7ca62a6681c15a69f5e7eea4e074de6fbec3
Shane Howley <shane@mainframe.com>
2017-12-11 18:13:50 +0000

    Ensure consistent encoding of hex strings

--- swarm/pss/types.go
+++ swarm/pss/types.go
@@ -1,10 +1,8 @@
 package pss
 
 import (
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"strings"
 	"sync"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -32,7 +30,7 @@ func (t *Topic) Unmarshal(input []byte) error {
 }
 
 func (t Topic) MarshalJSON() (b []byte, err error) {
-	return json.Marshal(hex.EncodeToString(t[:]))
+	return json.Marshal(hexutil.Encode(t[:]))
 }
 
 func (t *Topic) UnmarshalJSON(input []byte) error {
@@ -48,8 +46,7 @@ func (t *Topic) UnmarshalJSON(input []byte) error {
 type PssAddress []byte
 
 func (a PssAddress) MarshalJSON() ([]byte, error) {
-	s := strings.Join([]string{"\"", hexutil.Encode((a)[:]), "\""}, "")
-	return []byte(s), nil
+	return json.Marshal(hexutil.Encode(a[:]))
 }
 
 func (a *PssAddress) UnmarshalJSON(input []byte) error {
